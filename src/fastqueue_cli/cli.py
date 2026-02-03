417e62b75c9f774195d717ee7a4bd5554fde6783
Giorgi Merebashvili <mereba2627@gmail.com>
2026-02-03 23:55:34 +0400

    Add worker update command (#3)
    * Start working on update command
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Finish update

--- src/fastqueue_cli/cli.py
+++ src/fastqueue_cli/cli.py
@@ -3,7 +3,8 @@ from typing import Annotated
 
 import typer
 
-from fastqueue_cli.worker import download_and_install
+from fastqueue_cli.exceptions import InvalidVersionError
+from fastqueue_cli.worker import download_and_install, update_worker
 
 app = typer.Typer()
 worker_app = typer.Typer()
@@ -16,25 +17,46 @@ def worker_install(
     version: Annotated[
         str | None,
         typer.Option(
-            help="Exact version of the worker you want to install (e.g 0.1.0, 0.2.3)."
+            help="Specify a version to install. If not provided, installs the latest version."
         ),
     ] = None,
 ):
     """
-    Download and install [bold]FastQueue Worker[/bold].
+    Download and install [bold]fastqueue-worker[/bold].
     """
     if version:
         pattern = r"^\d+\.\d+\.\d+$"
 
         if not re.match(pattern, version):
-            raise Exception(
+            raise InvalidVersionError(
                 "Invalid version. Please use versions like: 0.1.0, 0.2.3"
             )
 
-    # TODO: Check if its already installed or not,
-    # if it is just print it and add `update` command to update the version
-    # NOTE: --version argument should force it
     download_and_install(version)
+
+
+@worker_app.command(name="update")
+def worker_update(
+    version: Annotated[
+        str | None,
+        typer.Option(
+            help="Specify a version to update to. If not provided, updates to the latest version."
+        ),
+    ] = None,
+    no_backup: Annotated[
+        bool,
+        typer.Option(
+            is_flag=True,
+            flag_value=False,
+            show_default=False,
+            help="Do not save the old version binary when updating. Default behavior saves it.",
+        ),
+    ] = False,
+):
+    """
+    Update [bold]fastqueue-worker[/bold].
+    """
+    update_worker(version=version, no_backup=no_backup)
 
 
 if __name__ == "__main__":
